5aa846190765830524616463715a277f25c31e7e
Visiona <darbis@gmail.com>
2016-09-11 23:40:23 +0100

    Finished Tower of Hanoi Game

--- tower.rb
+++ tower.rb
@@ -1,53 +1,71 @@
-=begin
 
-def play
 
+def render(rods, height)
+  puts "#{rods}"
+  fixed_height = height
+  rods_int = rods.dup
+  until height == 0
+    rods_int.each do |rod_no|
+      if rod_no[height-1] == nil
+        print ("  "*4 + "  "*fixed_height)
+      elsif rod_no[height-1].odd?
+        print ("  "*4 + "  "*((fixed_height - rod_no[height-1])/2) + "==" * rod_no[height-1] + "  "*((fixed_height-rod_no[height-1])/2))
+      else
+        print ("  "*4 + " "*1 + "  "*((fixed_height - rod_no[height-1])/2) + "==" * rod_no[height-1] + "  "*((fixed_height-rod_no[height-1])/2))
+      end
+    end
+    print "\n"
+    height -= 1
+  end
+  1.upto(3) {|i| print ("  "*4 + "  "*(fixed_height/2) + "#{i}" + "   ")}
+  print "\n"
+end
+
+def is_valid(move, height)
+  if move.length == 3 && (1..height) === move[0].to_i && (1..height) === move[2].to_i && "," == move[1]
+    true
+  else
+    puts "You have typed incorrect parameters."
+  end
 end
 
 def tower_of_hanoi(height)
 
+  #Creating initial rodes with disks
+  rod_1, rod_2, rod_3 = Array.new, Array.new, Array.new
+  height.downto(1) {|h| rod_1 << h}
+  rods = [rod_1, rod_2, rod_3]
+
+  puts "Welcome to Tower of Hanoi!"
+  puts "Instructions:"
+
+  until rod_1.empty? && (rod_2.empty? || rod_3.empty?)
+    puts "Enter where you'd like to move from and to in the format '1,3'."
+    puts "Enter 'q' to quit."
+    puts "Current Board:"
+    render(rods, height)
+    puts "Enter move >"
+    move = gets.chomp
+
+    if move == "q"
+      puts "Thank you for playing with us. Good Bye."
+      break
+    end
+
+    if is_valid(move, height)# checks if user typed exactly integer followed by coma and another integer instead of ie. true
+      move = move.split(",").map{|s| s.to_i}
+      if rods[move[0]-1].empty?
+        puts "You are trying to move disk from an empty rod. Please try again."
+      elsif rods[move[1]-1].length > 0
+        if rods[move[0]-1].last>rods[move[1]-1].last
+          puts "You can't put this disk on a smaller one. Please try again."
+        else
+          rods[move[1]-1] << rods[move[0]-1].pop # we are moving disk as instructed by the player
+        end
+      else
+        rods[move[1]-1] << rods[move[0]-1].pop
+      end
+    end
+    puts "Congratulations. You have won the game. Good Job!" if rod_1.empty? && (rod_2.empty? || rod_3.empty?)
+  end
 end
-=end
-height = 5
-puts "Welcome to Tower of Hanoi!"
-puts "Instructions:"
-puts "Enter where you'd like to move from and to in the format '1,3'."
-puts "Enter 'q' to quit."
-puts "Current Board:"
-
-#Full stack generated
-rod_1 = Array.new(height)
-rod_1.inject(height) {|total| total - 1}
-rod_2 = Array.new(height)
-rod_3 = Array.new(height)
-
-puts "#{rod_1}, #{rod_2}, #{rod_3}"
-
-puts "Enter move >"
-move = gets.chomp.split(",")
-
-
-
-# Current Board:
-# o
-# oo
-# ooo
-# 1    2    3
-# Enter move >
-# ...
-#def render - prints out the current state of the game board in between turns
-#Create a game loop that finishes when the user either quits (for instance, by entering quit on the input line) or wins.
-
-=begin
-#PSEUDOCODE
-1.Start the game by initiating height, hello message and rules
-2.Image of current Board so the correct is there
-3. Invitation to start the game or quit
-4. enter movement
-5. Again message appears to enter movement and image of current rod is seen
-6. If user enters quit - game end with adequate message
-7. If user enters movement frmo an empty rod or to a rod with a top smaller disk, repeats the message to enter and image
-8. If user enters incorrect movement - adequate message appears - Check for valid user inputs
-9. If all disks are moved to a different stack, winnig message appears - end of the game
-10 
-=end
